6a77f4b116611ae7aa26d80a35e810a9a44f93a7
kevinlee0929 <807536299@qq.comm>
2018-05-06 19:53:00 +0800

    second comm

--- secondTest.java
+++ secondTest.java
@@ -1,9 +1,6 @@
 public static void main(String args[]){
 	System.out.println("this is the second java test!");
-<<<<<<< HEAD
 	second local modify!
-=======
 	second test class kevin modify!
->>>>>>> d829d64ac58008c1614a12294cfc7e5fd66536df
 }
 
